daf9e0ca61e8363bae81e8bf5455276b9602977f
LEEBOHYUN <100012474+bohyun86@users.noreply.github.com>
2024-09-19 11:28:52 +0900

    9/19 하이버네이트, jpa 버전 변경... 버전 충돌 해결

--- BookMyShow/src/main/java/com/itwillbs/repository/MusicalRepository.java
+++ BookMyShow/src/main/java/com/itwillbs/repository/MusicalRepository.java
@@ -6,7 +6,6 @@ import com.itwillbs.domain.partner.PartnerDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.time.LocalDate;
 import java.util.List;
 
