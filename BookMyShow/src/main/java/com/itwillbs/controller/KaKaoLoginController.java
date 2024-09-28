53e63155fdf62e7534613d8e3262cead636d9b5e
bohyun <bohyun86@kakao.com>
2024-09-28 17:24:45 +0900

    9/28 카카오 간편로그인 / 회원가입 구현 중

---
+++ BookMyShow/src/main/java/com/itwillbs/controller/KaKaoLoginController.java
@@ -0,0 +1,14 @@
+package com.itwillbs.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+@Log4j2
+public class KaKaoLoginController {
+
+
+
+}
